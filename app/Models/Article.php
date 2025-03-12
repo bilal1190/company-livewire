0b20b96fa751b292d40881442c6bae87d4767c9f
bilal1190 <bilalhmz1190@gmail.com>
2025-03-13 00:33:27 +0500

    code is ok

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Article extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'content', 'image', 'status', 'category_id', 'author'];
+    protected $fillable = ['title', 'content', 'image', 'status', 'category_id' => 1, 'author'];
 }
